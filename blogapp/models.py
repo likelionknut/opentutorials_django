03214d5eeb80436db9439c64de8931abf03e28a3
knut-sdpark <sdp1422@naver.com>
2019-05-08 00:27:10 +0900

    modified pub_date 필드는 auto로 입력되도록 수정 & author 필드 추가 'models.py'

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length=100)
-    pub_date = models.DateTimeField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=True, null=True, default=1)
     body = models.TextField()
