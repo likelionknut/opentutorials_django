3af8c9520c1972d8a59a77b31f75b2e3d3fd5757
knut-sdpark <sdp1422@naver.com>
2019-05-08 10:46:15 +0900

    add 'ckeditor'에 대한 url path 추가 'urls.py'

--- firstProject/urls.py
+++ firstProject/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import blogapp.views
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blogapp.views.index, name='index'),
     path('blogMain/', blogapp.views.blogMain, name='blogMain'),
     path('blogMain/createBlog/', blogapp.views.createBlog, name='createBlog'),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
