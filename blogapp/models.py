5a79272776a9fd6b405c04e026b0605ad0a04f63
knut-sdpark <sdp1422@naver.com>
2019-05-08 09:55:22 +0900

    modified 'body'필드를 RichTextField로 수정 'models.py'

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length=100)
     pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=True, null=True, default=1)
-    body = models.TextField()
+    body = RichTextField()
