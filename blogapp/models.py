ecafb7aafd9e300ad68599acaab7be37fa4eb35e
knut-sdpark <sdp1422@naver.com>
2019-05-08 10:57:29 +0900

    modified 'body'->'RichTextUploadingField'로 수정 'models.py'

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
 
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length=100)
     pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=True, null=True, default=1)
-    body = RichTextField()
+    body = RichTextUploadingField()
