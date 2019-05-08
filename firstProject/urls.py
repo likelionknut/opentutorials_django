daaeb5b780552850cfa304291b423493dd043105
knut-sdpark <sdp1422@naver.com>
2019-05-08 10:52:34 +0900

    add 'MEDIA' 경로 참조 추가 'urls.py'

--- firstProject/urls.py
+++ firstProject/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 import blogapp.views
 from django.conf.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +27,5 @@ urlpatterns = [
     path('blogMain/createBlog/', blogapp.views.createBlog, name='createBlog'),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
